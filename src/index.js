c6682af1503372466a1f663d9609f79c4efa6ce3
L. Cadet <85368055+Ludcad615@users.noreply.github.com>
2022-04-13 11:52:21 -0400

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// write your code here
+
+
+
+
+/*getElementById('date').innerHTML = new Date().toDateString();
+  
+/*
+form.addEventListener('submit', (e) =>{
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) =>{
+form.addEventListener();
+ul.append(li);
+const button = document.createElement('button')
+
+form.reset();
+/*
+"new-name" = (Top Ramen, Red Ramen, Silk Ramen, Pung Ramen) 
+"new-restaurant" = (ABC Palace, LES Palace, Chef Palace, Rare Palace)
+"new-image"= ???
+"new-rating" (0,1,2,3,4,5)
+"new-comment"= ("it was great", "it was alright, "it was bad")
+
+document.addEventListener("click", function() {
+  myFunction(p1, p2);
+});
+
+document.addEventListener("mouseover", myFunction);
+document.addEventListener("click", someOtherFunction);
+document.addEventListener("mouseout", someOtherFunction);
